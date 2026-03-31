d92ca167a766d96400d8e1e7f69e96ab26b2dff7
JetpackJackson <jetpackjackson@katzenmue.de>
2026-03-31 12:53:22 -0400

    fmt, mv assets, cleanup, function stubs

--- src/main.py
+++ src/main.py
@@ -1,28 +1,41 @@
-def read_input_from_file(filepath): # read input helper function
-    with open(filepath, 'r') as f:
-        data = f.read().split('\n')
+# TODO: add way to pick file like in past assignments
+
+
+def read_input_from_file(filepath):  # read input helper function
+    with open(filepath, "r") as f:
+        data = f.read().split("\n")
     idx = 0
 
-    K = int(data[idx]); idx += 1
+    K = int(data[idx])
+    idx += 1
 
     char_values = {}
     for _ in range(K):
-        parts = data[idx].split(); idx += 1
+        parts = data[idx].split()
+        idx += 1
         char_values[parts[0]] = int(parts[1])
 
-    A = data[idx]; idx += 1
-    B = data[idx]; idx += 1
+    A = data[idx]
+    idx += 1
+    B = data[idx]
+    idx += 1
 
     return K, char_values, A, B
 
 
 def main():
-    # print("Hello from pa3!")
-    K, char_values, A, B = read_input_from_file("input.txt")
+    K, char_values, A, B = read_input_from_file("assets/input1.txt")
     print(f"K = {K}")
     print(f"char_values = {char_values}")
     print(f"A = {A}")
     print(f"B = {B}")
+    calculate(K, char_values, A, B)
+
+
+def calculate(K, char_values, A, B):
+    # TODO
+    raise NotImplementedError
+
 
 if __name__ == "__main__":
     main()
