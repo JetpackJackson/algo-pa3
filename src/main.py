cd0eb10c8f93ff71689d855cd0bbb07e66190231
JetpackJackson <jetpackjackson@katzenmue.de>
2026-03-31 16:15:53 -0400

    add calculations

--- src/main.py
+++ src/main.py
@@ -33,8 +33,41 @@ def main():
 
 
 def calculate(K, char_values, A, B):
-    # TODO
-    raise NotImplementedError
+    n = len(A)
+    m = len(B)
+
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    # literally just a recurrence relation
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if A[i - 1] == B[j - 1]:
+                # match: include me
+                dp[i][j] = dp[i - 1][j - 1] + char_values[A[i - 1]]
+            else:
+                # no match: max stuff
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    # backtrack
+    result: list[str] = []
+    i, j = n, m
+    while i > 0 and j > 0:
+        if A[i - 1] == B[j - 1]:
+            # part of opt soln
+            result.append(A[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+
+    # reverse order
+    result.reverse()
+
+    # result
+    print(dp[n][m])
+    print(str("".join(result)))
 
 
 if __name__ == "__main__":
